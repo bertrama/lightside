d34b17bd8c465f1c28e0156796adbb7e1c24a8fe
David Adamson <dadamson>
2011-06-06 12:14:47 -0400

    Added new Feature class ecosystem - untested.

---
+++ SIDE core/edu/cmu/side/feature/LocalFeatureHit.java
@@ -0,0 +1,33 @@
+package edu.cmu.side.feature;
+
+import java.util.Collection;
+
+/**
+ *  A feature hit with a particular location within a document
+ */
+public class LocalFeatureHit extends FeatureHit
+{
+	/**
+	 * {start, end} pairs indicating where this feature is expressed in this document.
+	 * end - start = length of hit.
+	 */
+	private Collection<int[]> hits;
+
+	/**
+	 * 
+	 * @param feature the feature which has hit the document in these spots.
+	 * @param value the value expressed by the feature for this document. Its type should agree with feature.getFeatureType()
+	 * @param documentIndex the index of the document in the current document set.
+	 * @param hits {start, end} pairs indicating exactly where this feature hits in this document.
+	 */
+	public LocalFeatureHit(Feature feature, Object value, int documentIndex, Collection<int[]> hits)
+	{
+		super(feature, value, documentIndex);
+		this.hits = hits;
+	}
+
+	public Collection<int[]> getHits()
+	{
+		return hits;
+	}
+}
--- SIDE core/edu/cmu/side/plugin/FEPlugin.java
+++ SIDE core/edu/cmu/side/plugin/FEPlugin.java
@@ -2,9 +2,14 @@ package edu.cmu.side.plugin;
 
 import java.io.File;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import edu.cmu.side.feature.*;
 import edu.cmu.side.uima.UIMAToolkit.DocumentList;
 
 /**
@@ -49,5 +54,37 @@ public abstract class FEPlugin extends SIDEPlugin {
 	public abstract String getOutputName();
 	
 	public abstract List<Map<String, Number>> extractFeatureMapsForSubclass(DocumentList documents) throws Exception;
+
+	public Collection<FeatureHit> extractFeatureHits(DocumentList documents)
+	{
+		// TODO make me abstract.
+		// DUCT TAPE: convert featureMaps to FeatureHits
+		
+		ArrayList<FeatureHit> hits = new ArrayList<FeatureHit>();
+		
+		try
+		{
+			HashMap<String, Feature> myFeatures = new HashMap<String, Feature>();
+			
+			List<Map<String, Number>> featureMaps = extractFeatureMapsForSubclass(documents);
+			for(int i = 0; i < documents.getSize(); i++)
+			{
+				for(Entry<String, Number> feature : featureMaps.get(i).entrySet())
+				{
+					String featureName = feature.getKey();
+					if(!myFeatures.containsKey(featureName))
+						myFeatures.put(featureName, new Feature(getOutputName(), featureName, Feature.Type.NUMERIC));
+					
+					hits.add(new FeatureHit(myFeatures.get(featureName), feature.getValue(), i));
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return hits;
+	}
 
 }

e38976d35e6fd57abbb2cdfbb03a368f6445d119
kertrats <none@none>
2011-07-13 15:14:26 -0400

    Adding improved TableModel class that allows better sorting.

---
+++ SIDE core/edu/cmu/side/simple/newui/EvalTableModel.java
@@ -0,0 +1,13 @@
+package edu.cmu.side.simple.newui;
+
+import javax.swing.table.DefaultTableModel;
+
+/** This allows us to sort numerically, not just by toString() */
+public class EvalTableModel extends DefaultTableModel{
+	private static final long serialVersionUID = -6623645069818166916L;
+
+	@Override
+	public Class<?> getColumnClass(int col){
+		return (col==1?Double.class:Object.class);
+	}
+}

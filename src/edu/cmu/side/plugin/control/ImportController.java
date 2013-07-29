4c010cb4c51e5933bb3b6493c871e46366bb8ee0
alatterner <none@none>
2013-07-29 18:48:34 -0400

    File Parser and ImportController updated to throw IOExceptions. Generic load panel has a new if check for IOException.

--- src/edu/cmu/side/plugin/control/ImportController.java
+++ src/edu/cmu/side/plugin/control/ImportController.java
@@ -1,5 +1,8 @@
 package edu.cmu.side.plugin.control;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,7 +27,7 @@ public class ImportController {
 		return foundParser;
 	}
 	
-	public static DocumentList makeDocumentList(TreeSet<String> fileNames) throws Exception{
+	public static DocumentList makeDocumentList(TreeSet<String> fileNames) throws Exception, IOException, FileNotFoundException{
 		if(fileNames.size()==0) return null;
 		fileChunks = new HashMap<FileParser, HashSet<String>>();
 		if(parsers.length==0){
@@ -33,9 +36,13 @@ public class ImportController {
 		for (SIDEPlugin parser : parsers) {
 			fileChunks.put((FileParser)parser, new HashSet<String>());
 		}
-		for (String file : fileNames) {
-			if(!getValidPlugin(file)){
-				throw new Exception("File: " + file.toString() + " could not be parsed.");
+		for (String fileName : fileNames) {
+			File file = new File(fileName);
+			if(!file.exists()){
+				throw new FileNotFoundException(fileName);
+			}
+			else if(!getValidPlugin(fileName)){
+				throw new Exception("File: " + fileName.toString() + " could not be parsed.");
 			}
 		}
 		ArrayList<DocumentList> toAggregate = new ArrayList<DocumentList>();
--- src/edu/cmu/side/view/generic/GenericLoadPanel.java
+++ src/edu/cmu/side/view/generic/GenericLoadPanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -369,7 +370,9 @@ public abstract class GenericLoadPanel extends AbstractListPanel
 		testDocs.guessTextAndAnnotationColumns();
 		Recipe r = Workbench.getRecipeManager().fetchDocumentListRecipe(testDocs);
 		setHighlight(r);
-		} catch(Exception e){
+		}catch(FileNotFoundException e){
+			JOptionPane.showMessageDialog(this, e.getMessage(), "File Not Found", JOptionPane.ERROR_MESSAGE);
+		}catch(Exception e){
 			JOptionPane.showMessageDialog(this, e.getMessage(), "Load Error", JOptionPane.ERROR_MESSAGE);
 			e.printStackTrace();
 		}

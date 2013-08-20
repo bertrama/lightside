b7a87b2d3ecbeeab6352526f56deee7939950fc4
alatterner <none@none>
2013-08-20 11:20:44 -0400

    Updated ConverterControl with Zip stuff
    --HG--
    branch : recipe-save-system

--- src/edu/cmu/side/recipe/converters/ConverterControl.java
+++ src/edu/cmu/side/recipe/converters/ConverterControl.java
@@ -1,29 +1,28 @@
 package edu.cmu.side.recipe.converters;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import com.thoughtworks.xstream.XStream;
 
 import edu.cmu.side.model.Recipe;
-import edu.cmu.side.model.data.DocumentList;
 
 public class ConverterControl {
-	public static DocumentList docList;
-//	public static
-	//whatever else should get repeated here
 	private ConverterControl(){
 		
 	}
 	
 	public static void writeToXML(String fileName, Recipe recipe){
-		clearVariables();
 		File file = createFile(fileName);
 		XStream stream = new XStream();
-		stream.registerConverter(new RecipeConverter());
+		stream.registerConverter(new FeatureTableConverter());
 		try {
 			FileOutputStream out = new FileOutputStream(file);
 			PrintStream printStream = new PrintStream(out);
@@ -36,10 +35,9 @@ public class ConverterControl {
 	}
 	
 	public static Recipe readFromXML(String fileName){
-		clearVariables();
 		File file = createFile(fileName);
 		XStream stream = new XStream();
-		stream.registerConverter(new RecipeConverter());
+		stream.registerConverter(new FeatureTableConverter());
 		Recipe r =(Recipe) stream.fromXML(file);
 		return r;
 	}
@@ -55,9 +53,52 @@ public class ConverterControl {
 			}
 		return file;
 	}
-	
-	private static void clearVariables(){
-		docList = null;
-		
+	//Let's hook these up
+	private void zipUp(String fileName){
+		long start = System.currentTimeMillis();
+		byte[] buff = new byte[1000];
+		FileOutputStream stream;
+		try {
+			stream = new FileOutputStream("testest.zip");
+			ZipOutputStream zipper = new ZipOutputStream(stream);
+			ZipEntry entr = new ZipEntry("testest.xml");
+			zipper.putNextEntry(entr);
+			FileInputStream in = new FileInputStream("testest.xml");
+			int len;
+			while((len=in.read(buff))>0){
+				zipper.write(buff, 0, len);
+			}
+			in.close();
+			zipper.closeEntry();
+			zipper.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("ziptime : " + (end-start));
 	}
+	private void unZip() throws IOException{
+		long start = System.currentTimeMillis();
+		byte[] buffer = new byte[1000];
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(new File("testest.zip")));
+		ZipEntry entr = zis.getNextEntry();
+		File newFile = new File(entr.getName());
+		if(!newFile.exists()){
+			newFile.createNewFile();
+		}
+		FileOutputStream fos = new FileOutputStream(newFile);             
+		 
+        int len;
+        while ((len = zis.read(buffer)) > 0) {
+   		fos.write(buffer, 0, len);
+        }
+
+        fos.close();   
+        zis.closeEntry();
+        zis.close();
+        long end = System.currentTimeMillis();
+		System.out.println("unziptime : " + (end-start));
+	}
+	
 }

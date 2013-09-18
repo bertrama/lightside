29bd8ce5346ac4d21491d2d600103695e7c89296
David Adamson <dadamson@cs.cmu.edu>
2013-09-18 11:24:31 -0400

    updated chefs for templated model building
    --HG--
    branch : fall-2013

--- src/edu/cmu/side/recipe/Chef.java
+++ src/edu/cmu/side/recipe/Chef.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -14,6 +16,8 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import javax.swing.JOptionPane;
+
+import plugins.metrics.models.BasicModelEvaluations;
 
 import com.sun.xml.internal.ws.encoding.soap.DeserializationException;
 
@@ -37,6 +41,11 @@ import edu.cmu.side.plugin.SIDEPlugin;
  */
 public class Chef
 {
+    static { 
+        System.setProperty("java.awt.headless", "true");
+        System.out.println(java.awt.GraphicsEnvironment.isHeadless()?"Running in headless mode.":"Not actually headless");
+      }
+    
 	static boolean quiet = false;
 
 	static StatusUpdater textUpdater = new StatusUpdater()
@@ -73,6 +82,7 @@ public class Chef
 		
 		for (SIDEPlugin plug : extractors.keySet())
 		{
+			if(!quiet) System.out.println("Extracting features with "+plug+"...");
 			//System.out.println("Extractor Settings: "+extractors.get(plug));
 			Collection<FeatureHit> extractorHits = ((FeaturePlugin) plug).extractFeatureHits(corpus, extractors.get(plug), textUpdater);
 			hits.addAll(extractorHits);
@@ -85,7 +95,8 @@ public class Chef
 		{
 			for (SIDEPlugin plug : recipe.getFilters().keySet())
 			{
-				ft = ((RestructurePlugin) plug).filterTestSet(recipe.getTrainingTable(), ft, recipe.getFilters().get(plug), textUpdater);
+				if(!quiet) System.out.println("Restructuring features with "+plug+"...");
+				ft = ((RestructurePlugin) plug).restructure(recipe.getTrainingTable(), recipe.getFilters().get(plug), textUpdater);
 			}
 			recipe.setFilteredTable(ft);
 		}
@@ -98,6 +109,7 @@ public class Chef
 		
 		prepareDocumentList(originalRecipe, corpus);
 		newRecipe.setDocumentList(corpus);
+		printMemoryUsage();
 		
 		if(finalStage == Stage.DOCUMENT_LIST)
 			return newRecipe;
@@ -109,11 +121,13 @@ public class Chef
 			annotation = null;
 		
 		simmerFeatures(newRecipe, newThreshold, annotation, originalFeatures.getClassValueType());
+		printMemoryUsage();
 		
 		if(finalStage.compareTo(Stage.TRAINED_MODEL) < 0)
 			return newRecipe;
 		
 		broilModel(newRecipe);
+		printMemoryUsage();
 		
 		return newRecipe;
 	}
@@ -126,6 +140,7 @@ public class Chef
 	//Build Model
 	protected static void broilModel(Recipe newRecipe) throws Exception
 	{
+		if(!quiet) System.out.println("Training model with "+newRecipe.getLearner()+"...");
 		TrainingResult trainResult = newRecipe.getLearner().train(newRecipe.getTrainingTable(), newRecipe.getLearnerSettings(), newRecipe.getValidationSettings(), newRecipe.getWrappers(), textUpdater);
 		newRecipe.setTrainingResult(trainResult);
 		
@@ -139,6 +154,7 @@ public class Chef
 	 */
 	protected static void prepareDocumentList(Recipe originalRecipe, DocumentList corpus)
 	{
+		if(!quiet) System.out.println("Preparing documents...");
 		DocumentList original = originalRecipe.getDocumentList();
 		FeatureTable originalTable = originalRecipe.getTrainingTable();
 		String currentAnnotation = originalTable.getAnnotation();
@@ -148,7 +164,7 @@ public class Chef
 		}
 		else
 		{
-//			System.err.println("Warning: data has no "+currentAnnotation+" annotation. You can't train a new model on this data (only predict)");
+			System.err.println("Warning: data has no "+currentAnnotation+" annotation. You can't train a new model on this data (only predict)");
 		}
 		corpus.setLabelArray(originalTable.getLabelArray());
 		corpus.setTextColumns(new HashSet<String>(original.getTextColumns()));
@@ -198,35 +214,76 @@ public class Chef
 
 	public static void main(String[] args) throws Exception
 	{
-		String recipePath = "saved/bayes.model.side";
-		if (args.length < 1)
+		
+		String recipePath, outPath;
+		if (args.length < 2)
 		{
-			System.err.println("usage: chef.sh path/to/my.model.side corpus.csv...");
+			System.err.println("usage: chef.sh saved/template.model.side saved/new.model.side data.csv...");
+			return;
 		}
-		else
-			recipePath = args[0];
+		
+		recipePath = args[0];
+		outPath = args[1];
 
 		Set<String> corpusFiles = new HashSet<String>();
 		
 		String dataFile = "data/MovieReviews.csv";
-		if (args.length < 2) corpusFiles.add(dataFile);
-		else for(int i = 1; i < args.length; i++)
+		if (args.length < 3) corpusFiles.add(dataFile);
+		else for(int i = 2; i < args.length; i++)
 		{
 			corpusFiles.add(args[i]);
 		}
 
+		if(!quiet) System.out.println("Loading "+recipePath);
 		Recipe recipe = loadRecipe(recipePath);
-		Recipe result = followRecipe(recipe, new DocumentList(corpusFiles), recipe.getStage(), recipe.getFeatureTable().getThreshold());
+		printMemoryUsage();
 
-		System.out.println("extracted "+result.getFeatureTable().getFeatureSet().size()+" features.");
+		if(!quiet) System.out.println("Loading documents: "+corpusFiles);
+		Recipe result = followRecipe(recipe, new DocumentList(corpusFiles), recipe.getStage(), recipe.getFeatureTable().getThreshold());
+		
 		if(result.getStage().compareTo(Stage.TRAINED_MODEL) >= 0)
-			System.out.println(result.getTrainingResult().getTextConfusionMatrix());
+		{
+
+			displayTrainingResults(result);
+		}
+
+
+		System.out.println("Saving finished recipe to "+outPath);
+		saveRecipe(result, new File(outPath));
 		
 	}
 
+	protected static void printMemoryUsage()
+	{
+		if(quiet) return;
+		
+		double gigs = 1024 * 1024 * 1024;
+		MemoryUsage usage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+
+		double beanMax = usage.getMax() / gigs;
+		double beanUsed = usage.getUsed() / gigs;
+
+		System.out.println(String.format("%.1f/%.1f GB used", beanUsed, beanMax));
+	}
+
+	/**
+	 * @param recipe
+	 */
+	protected static void displayTrainingResults(Recipe recipe)
+	{
+		if(recipe.getStage().compareTo(Stage.TRAINED_MODEL) >= 0)
+		{
+			TrainingResult trainingResult = recipe.getTrainingResult();
+			System.out.println(trainingResult.getTextConfusionMatrix());
+			BasicModelEvaluations eval = new BasicModelEvaluations();
+			System.out.println("Accuracy\t"+eval.getAccuracy(trainingResult));
+			System.out.println("Kappa\t"+eval.getKappa(trainingResult));
+		}
+	}
+	
 	public static void setQuiet(boolean b)
 	{
-		quiet = true;
+		quiet = b;
 	}
 
 }

26b6cceec03cb1bf49e0e6b609f9f779f7dcc50d
David Adamson <david@lightsidelabs.com>
2014-01-31 15:27:28 -0500

    radiobuttonlistentries were not sortable. They are, now
    --HG--
    branch : Document List Blues

--- src/edu/cmu/side/view/util/RadioButtonListEntry.java
+++ src/edu/cmu/side/view/util/RadioButtonListEntry.java
@@ -2,9 +2,8 @@ package edu.cmu.side.view.util;
 
 import javax.swing.JRadioButton;
 
-public class RadioButtonListEntry extends JRadioButton{
-
-
+public class RadioButtonListEntry extends JRadioButton implements Comparable
+{
 	  private Object value = null;
 
 	  public RadioButtonListEntry(Object itemValue, boolean selected) {
@@ -27,4 +26,10 @@ public class RadioButtonListEntry extends JRadioButton{
 		  if(value == null) return "NULL";
 		  return value.toString();
 	  }
+
+	@Override
+	public int compareTo(Object o)
+	{
+		return this.toString().compareTo(o.toString());
+	}
 }
